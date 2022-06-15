e39183e6593284efd4ac17678b3b0b9949f12e77
VeeWanjiru <88655198+VeeWanjiru@users.noreply.github.com>
2022-06-15 10:32:39 +0300

    Add files via upload

---
+++ WebApp-master/WebApp-master/loginchk.php
@@ -0,0 +1,40 @@
+<!--
+ * File : loginchk.php
+ * Developer : Agney Patel
+ * Website : www.agney.vishwasetu.com
+ * Copyright © Agney Patel 2016
+ -->
+
+<?php
+session_start();
+$error = '';
+
+if (isset($_POST['submit'])) {
+    if (empty($_POST['email']) || empty($_POST['password'])) {
+        echo "Email or Password is invalid";
+    }
+    else
+    {
+        $connection = mysqli_connect("localhost", "root", "root", "db");
+        $email      = $_POST['email'];
+        $password   = $_POST['password'];
+        // $email      = stripslashes($email);
+        // $password   = stripslashes($password);
+        // $email      = mysqli_real_escape_string($connection, $_REQUEST['email']);
+        // $password   = mysqli_real_escape_string($connection, $_REQUEST['password']);
+        $sql        = "SELECT * from userdata where password='$password' AND email='$email'";
+        $r_query    = mysqli_query($connection, $sql);
+        $rows       = mysqli_num_rows($r_query);
+
+        if ($rows == 1) {
+            $_SESSION['login_user'] = $email;
+            echo "<script>location='profile.php'</script>";
+        }
+        else {
+            $error = "Email or Password is invalid";
+        }
+
+        mysqli_close($connection);
+    }
+}
+?>
